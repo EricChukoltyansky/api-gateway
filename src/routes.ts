6662787b25a94f258d5cc8398f4c4b8cea48b4cf
Eric Chukoltyansky <erik.chukoltyansky@gmail.com>
2023-10-03 15:28:37 +0300

    updated routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,5 @@
+import rateLimit from "express-rate-limit";
+
 export type Route = {
   url: string;
   auth: boolean;
@@ -37,10 +39,88 @@ export const ROUTES: Route[] = [
         "^/api/products": "/products",
       },
     },
+  },
+  {
+    url: "/api/orders/:userId",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/orders": "/orders",
+      },
+    },
     rateLimit: {
       windowMs: 15 * 60 * 1000,
       max: 5,
       message: "Too many requests",
     },
   },
+  {
+    url: "/api/shop_inventory",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/shop_inventory": "/shop_inventory",
+      },
+    },
+  },
+  {
+    url: "/api/shop_inventory/:productId",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/shop_inventory": "/shop_inventory",
+      },
+    },
+  },
+  {
+    url: "/api/shop_inventory/updateInventory",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/shop_inventory/updateInventory": "/shop_inventory/updateInventory",
+      },
+    },
+  }, 
+  {
+    url: "/api/banner",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/banner": "/banner",
+      },
+    },
+  },
+  {
+    url: "/api/banner/:bannerId",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/banner": "/banner",
+      },
+    },
+  }, 
+  {
+    url: "/api/banner/product/:productId",
+    auth: false,
+    proxy: {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/banner/product": "/banner/product",
+      },
+    },
+  },
+  
 ];
